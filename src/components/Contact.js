c4c3c27236c6086d611f553aaf0bf5d9045705ea
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-12 13:17:54 +0800

    xd

---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+// src/components/Contact.js
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <h1>Contact Us</h1>
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Email:
+                    <input type="email" name="email" />
+                </label>
+                <label>
+                    Message:
+                    <textarea name="message" />
+                </label>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
+};
+
+export default Contact;
